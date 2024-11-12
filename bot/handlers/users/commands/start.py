ccdc4fb58d4ca1538f532553b1314555451810c3
Nikita <nikita.gubarev31@gmail.com>
2024-11-12 11:20:00 +0200

    Update the start command handler message

--- bot/handlers/users/commands/start.py
+++ bot/handlers/users/commands/start.py
@@ -13,13 +13,15 @@ router = Router()
 
 @router.message(CommandStart())
 @clear_state_before
-async def handle_start_command(message: Message, *args):
+async def handle_start_command(message: Message):
     """Handles the /start command."""
-    await message.answer_sticker("")
+    await message.answer_sticker(
+        "CAACAgIAAxkBAAENG_pnMx2l0j2ceR1pPO86hRDoAAH9Fj4AAisHAAJjK-IJn8R5tQ5lfPI2BA"
+    )
     await message.answer(
         _(
             "Привіт, {name}!\n"
-            "Я той, хто допоможе Вам "
+            "Я той, хто допоможе Вам знайти резюме з популярних сайтів роботи."
         ).format(name=message.from_user.full_name)
     )
     await handle_menu(message)
